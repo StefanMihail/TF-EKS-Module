9ba192a1ae3277507caba72479c5960361d6ce9c
Stefan Cociobea <stefan.cociobea@allcloud.io>
2020-01-12 01:16:02 +0200

    commit to Feature

--- variables.tf
+++ variables.tf
@@ -5,43 +5,48 @@
 #var.environment
 variable "environment" {
   type    = "string"
+  default = "Development"
 }
 
 #var.customer
 variable "customer" {
   type    = "string"
+  default = "Omni-Tech"
 }
 
 #var.cluster-name
 variable "cluster-name" {
   type    = "string"
+  default = "omni-tech.org"
 }
 
 #var.vpc_id
 variable "vpc_id" {
-  default = ""
+  default = "vpc-0127ff98c92a36c9a"
   type    = "string"
 }
 
 #var.master_subnet_ids
 variable "master_subnet_ids" {
   type    = "list"
+  default = ["subnet-07d2ac82356e01fe9","subnet-0cd2e8fc00aee53ba"]
 }
 
 #var.node_subnet_ids
 variable "node_subnet_ids" {
   type    = "list"
+    default = ["subnet-07d2ac82356e01fe9","subnet-0cd2e8fc00aee53ba", "subnet-0e6f8ecea470be6e0"]
 }
 
 #var.instance_type
 variable "instance_type" {
-  default = "m4.large"
+  default = "t2.micro"
   type    = "string"
 }
 
 #var.ssh_key_name
 variable "ssh_key_name" {
-  default = ""
+  default = "v2"
 }
 
 #var.root_volume_size
@@ -57,19 +62,19 @@ variable "min_nodes" {
 
 #var.max_nodes
 variable "max_nodes" {
-  default = "10"
+  default = "4"
   type    = "string"
 }
 
 #var.vpn_ssl_pool
 variable "vpn_ssl_pool" {
   description = "The VPN SSL pool, to allow SSH from"
-  default     = []
+  default     = ["0.0.0.0/0	"]
   type    = "list"
 }
 
 #var.k8s_version
 variable "k8s_version" {
   description = "Version of K8s for node's AMI to use"
-  default     = "1.11"
+  default     = "1.14"
 }
